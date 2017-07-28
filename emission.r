850966c51da5a63a8dd3366bbf0938c8c37abf2b
philhouse <mai11bli@studserv.uni-leipzig.de>
2017-07-28 17:06:27 +0200

    added emission data conversion from c to co2 at import

--- emission.r
+++ emission.r
@@ -8,6 +8,9 @@ read_co2_global = function(path) {
   sdf_co2_global <- rename(sdf_co2_global, Total_in_mega_tons = 
                              Total_carbon_emissions_from_fossil_fuel_consumption_and_cement_production_million_metric_tons_of_C)
   sdf_co2_global <- sdf_co2_global %>% select(Year, Total_in_mega_tons)
-  sdf_co2_global <- sdf_co2_global %>% mutate(Total_in_mega_tons = as.numeric(Total_in_mega_tons))
+  # Emission estimates are saved in million metric tons of carbon. To
+  # convert these estimates to units of carbon dioxide (CO2), simply multiply
+  # these estimates by 3.667.
+  sdf_co2_global <- sdf_co2_global %>% mutate(Total_in_mega_tons = as.numeric(Total_in_mega_tons) * 3.667)
   sdf_co2_global <- sdf_co2_global %>% mutate(Year = as.integer(Year))
 }
